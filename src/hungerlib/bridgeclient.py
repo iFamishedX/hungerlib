bbdb5724425205902c29fa0cbbfd4fe8581a3a02
iFamishedX <me@ifamished.com>
2026-05-24 05:54:37 +0000

    Added support for HungerBridge v2.0

--- src/hungerlib/bridgeclient.py
+++ src/hungerlib/bridgeclient.py
@@ -4,7 +4,6 @@ from .utils.exceptions import HungerBridgeError, InvalidLevelError
 
 
 class BridgeClient:
-    '''HungerBridge client. Usually used internally, but can be used externally.'''
     def __init__(self, url: str, token: str):
         self.base = url.rstrip('/')
         self.headers = {
@@ -12,11 +11,14 @@ class BridgeClient:
             'Content-Type': 'application/json'
         }
 
-    # internal functions
+        # Detect v2 support once
+        self._v2 = self._detect_v2()
+
+    # internal helpers
     def _post(self, path, payload):
         r = requests.post(f'{self.base}{path}', headers=self.headers, json=payload)
         if not r.ok:
-            raise RuntimeError(f'HungerBridge error {r.status_code}: {r.text}')
+            raise HungerBridgeError(f'HungerBridge error {r.status_code}: {r.text}')
         try:
             return r.json()
         except:
@@ -25,11 +27,18 @@ class BridgeClient:
     def _get(self, path):
         r = requests.get(f'{self.base}{path}', headers=self.headers)
         if not r.ok:
-            raise RuntimeError(f'HungerBridge error {r.status_code}: {r.text}')
+            raise HungerBridgeError(f'HungerBridge error {r.status_code}: {r.text}')
         try:
             return r.json()
         except:
             return r.text
+
+    def _detect_v2(self) -> bool:
+        try:
+            resp = self._get('/v2/ping')
+            return isinstance(resp, dict) and resp.get('ok') is True
+        except:
+            return False
 
 
     # public api
@@ -40,14 +49,13 @@ class BridgeClient:
         silent: bool = False,
         normalize: bool = True
     ):
-        '''
-        Runs a command on the server running HungerBridge. Paramaters:
-        - command: the command to run on the server
-        - show_console: if the command should be shown on the Minecraft server
-        - silent: if the command should not return output
-        - normalize: if the json should be returned as a string
-        '''
-        data = self._post('/v1/run', {
+        """
+        Runs a command on the server.
+        Automatically uses v2 if available, otherwise v1.
+        """
+        path = '/v2/run' if self._v2 else '/v1/run'
+
+        data = self._post(path, {
             'command': command,
             'silent': silent,
             'show_console': show_console
@@ -68,33 +76,56 @@ class BridgeClient:
         return None
 
     def log(self, message: str, level: str = 'info') -> dict:
-        '''
-        Log something on the server. Levels:
-        - info
-        - warn
-        - error
-        - None
-        '''
+        """
+        Logs a message to the server
+        """
         valid_levels = ['info', 'warn', 'error', None]
         if level not in valid_levels:
             raise InvalidLevelError(f'"{level}" is not a valid log level')
 
+        path = '/v2/log' if self._v2 else '/v1/log'
         if level is not None:
-            return self._post('/v1/log', {
+            return self._post(path, {
                 'level': level,
                 'message': message
             })
         else:
-            no_level_message = f'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b{message}'      # uses backspaces to remove level
-            return self._post('/v1/log', {
+            no_level_message = (
+                '\b' * 20 + message     # removes level (crude method, may change later)
+            )
+            return self._post(path, {
                 'level': 'info',
                 'message': no_level_message
             })
 
+    def ping(self) -> dict:
+        """
+        Returns latency info
+        """
+        if not self._v2:
+            raise HungerBridgeError("The ping endpoint is not supported by this server")
+        return self._get('/v2/ping')
+
+    def getInfo(self) -> dict:
+        """
+        Returns bridge info
+        """
+        if not self._v2:
+            raise HungerBridgeError("The info endpoint is not supported by this server")
+        return self._get('/v2/info')
+
     def getStatus(self) -> dict:
-        '''Returns the status of the server'''
+        """
+        Returns server status
+        """
+        if self._v2:
+            return self._get('/v2/status')
         return self._get('/v1/status')
 
     def getVersion(self) -> dict:
-        '''Returns the version of the bridge and server'''
+        """
+        Returns version info
+        """
+        if self._v2:
+            return self.getInfo()
         return self._get('/v1/version')
